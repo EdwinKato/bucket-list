654fc9481cec9bb19e03dcaadbd31a43e9a3c726
Edwin Kato <edwin.kato@andela.com>
2017-07-05 20:45:55 +0300

    Add flask migrate and test command to manage.py file

--- manage.py
+++ manage.py
@@ -1,8 +1,35 @@
+import os
+
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-from app import app
+from api import create_app, db
 
-manager = Manager(app)
+application = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+migrate = Migrate(application, db)
+
+manager = Manager(application)
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.command
+def recreate_db():
+    """
+    Recreates a local database. Drops and creates a new database
+    """
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
 
 if __name__ == "__main__":
     manager.run()
