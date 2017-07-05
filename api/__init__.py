eb9f9d8bfa5ea278e1fb39c59ed660a223b1f6a9
Edwin Kato <edwin.kato@andela.com>
2017-07-05 20:39:50 +0300

    Add flask api app creation to init

---
+++ api/__init__.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+import connexion
+
+from config import config
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = connexion.FlaskApp(__name__, specification_dir='swagger/')
+    app.add_api('swagger.yaml')
+    application = app.app
+    application.config.from_object(config[config_name])
+    db.init_app(application)
+
+    return application
+
+from api.api import *
