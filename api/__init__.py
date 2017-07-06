ff61fb41273b8bf94bd0c64ddb1a4c1e1c91bb5f
Edwin Kato <edwin.kato@andela.com>
2017-07-07 01:48:39 +0300

    Add url rules for public unauthorised routes

--- api/__init__.py
+++ api/__init__.py
@@ -11,6 +11,15 @@ def create_app(config_name):
     app.add_api('swagger.yaml')
     application = app.app
     application.config.from_object(config[config_name])
+
+    application.add_url_rule('/auth/register',
+                             'register',
+                             register)
+
+    application.add_url_rule('/auth/login',
+                             'login',
+                             login)
+
     db.init_app(application)
 
     return application
