c48f06598cdb39a8915c585219b2e70a89af475d
Edwin Kato <edwin.kato@andela.com>
2017-07-06 23:06:55 +0300

    Add test for updating bucket list item

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -202,9 +202,34 @@ class TestApi(TestCase):
         self.assertIn('Wrong turn 6', str(response.data))
         self.assertIn('How i met your mother', str(response.data))
 
-    @unittest.skip("")
     def test_update_bucket_list_item(self):
-        pass
+        bucket_list_one = {
+            "description": "Movies i have to watch by the end of the week",
+            "status": "Pending",
+            "title": "Entertainment",
+            "user_id": 1
+        }
+        self.client.post('/api/v1/bucketlists',
+                         data=json.dumps(bucket_list_one),
+                         content_type='application/json')
+        item_one = {
+            "description": "Horror movies",
+            "status": "Pending",
+            "title": "Wrong turn 6"
+        }
+        self.client.post('/api/v1/bucketlists/1/items',
+                         data=json.dumps(item_one),
+                         content_type='application/json')
+        item_one_modifoied = {
+            "description": "Horror movies",
+            "status": "Pending",
+            "title": "The walking dead"
+        }
+        response = self.client.put('/api/v1/bucketlists/1/items/1',
+                                   data=json.dumps(item_one_modifoied),
+                                   content_type='application/json')
+        self.assertNotIn('Wrong turn 6', str(response.data))
+        self.assertIn('The walking dead', str(response.data))
 
     @unittest.skip("")
     def test_delete_bucket_list_item(self):
