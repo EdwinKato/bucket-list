3d2303d37bdcc9af5a4eaf237e7e3dfebd7a5ed1
Edwin Kato <edwin.kato@andela.com>
2017-07-06 23:19:51 +0300

    Add test for deleting bucket list item

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -3,7 +3,7 @@ from flask_testing import TestCase
 from flask import current_app, request, abort, jsonify, g, url_for
 
 from api.api import *
-from api.models import User, BucketList
+from api.models import User, BucketList, Item
 from api import create_app, db
 import json
 
@@ -231,6 +231,26 @@ class TestApi(TestCase):
         self.assertNotIn('Wrong turn 6', str(response.data))
         self.assertIn('The walking dead', str(response.data))
 
-    @unittest.skip("")
     def test_delete_bucket_list_item(self):
-        pass
+        bucket_list_one = {
+            "description": "Movies i have to watch by the end of the week",
+            "status": "Pending",
+            "title": "Entertainment",
+            "user_id": 1
+        }
+        self.client.post('/api/v1/bucketlists',
+                         data=json.dumps(bucket_list_one),
+                         content_type='application/json')
+        item_one = {
+            "description": "Horror movies",
+            "status": "Pending",
+            "title": "Wrong turn 6"
+        }
+        self.client.post('/api/v1/bucketlists/1/items',
+                         data=json.dumps(item_one),
+                         content_type='application/json')
+        count = len(Item.query.all())
+        response = self.client.delete('/api/v1/bucketlists/1/items/1')
+        new_count = len(Item.query.all())
+        self.assertEqual(new_count-count, -1)
+        self.assertEqual(response.status_code, 200)
