cb76bccc3bbed68dc49364a916b274ff6c3aa76a
Edwin Kato <edwin.kato@andela.com>
2017-07-06 11:53:17 +0300

    Implement register user api method

--- api/api.py
+++ api/api.py
@@ -1,19 +1,59 @@
 from connexion.resolver import RestyResolver
-from flask import current_app, request, abort, jsonify, g, url_for
+from flask import request, abort, jsonify, g, url_for
 from flask_httpauth import HTTPAuth
+import re
 
 __all__ = ["login", "register", "add_bucket_list", "get_bucket_lists",
            "get_bucket_list","put_bucket_list","delete_bucket_list",
            "create_item_in_bucket_list", "get_items_in_bucket_list",
            "update_bucket_list_item", "delete_bucket_list_item"]
 
+EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
 
-def login():
+
+def login(username, password):
     pass
 
 
 def register():
-    pass
+    data = request.get_json()
+    first_name = data['first_name']
+    last_name = data['last_name']
+    email = data['email']
+    username = data['username']
+    password = data['password']
+
+    user = User(first_name, last_name, email, username)
+    user.hash_password(password)
+
+    if not first_name.isalpha():
+        response = jsonify({'message': 'First name must be string alphabet type'})
+        response.status_code = 400
+        return response
+
+    if not last_name.isalpha():
+        response = jsonify({'message': 'Last name must be string alphabet type'})
+        response.status_code = 400
+        return response
+
+    if not username or not password:
+        response = jsonify({'error': 'Username or password should not be left blank'})
+        response.status_code = 400
+        return response
+
+    if not EMAIL_REGEX.match(email):
+        response = jsonify({'message': 'Please specify valid email address'})
+        return response
+
+    if len(password) < 6:
+        response = jsonify({'error': 'Password is too short, it must be more than 6 characters'})
+        response.status_code = 401
+        return response
+
+    user.hash_password(data['password'])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'message': 'User has been successfully created'})
 
 
 def add_bucket_list():
@@ -24,7 +64,7 @@ def get_bucket_lists():
     pass
 
 
-def get_bucket_list():
+def get_bucket_list(id):
     pass
 
 
@@ -52,3 +92,4 @@ def delete_bucket_list_item():
     pass
 
 from api.models import User, BucketList, Item
+from api  import db
