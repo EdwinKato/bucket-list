996cbb7eb5034fbc577cf68a4d0fe47cc2dc8303
Edwin Kato <edwin.kato@andela.com>
2017-07-06 21:27:22 +0300

    Add test for add bucket list

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -3,7 +3,7 @@ from flask_testing import TestCase
 from flask import current_app, request, abort, jsonify, g, url_for
 
 from api.api import *
-from api.models import User
+from api.models import User, BucketList
 from api import create_app, db
 import json
 
@@ -25,6 +25,18 @@ class TestApi(TestCase):
         # }
         db.create_all()
 
+        self.test_user = {
+            "email": "edwin.kato@andela.com",
+            "first_name": "Edwin",
+            "last_name": "Kato",
+            "password": "qwerty@123",
+            "username": "EdwinKato"
+        }
+
+        self.client.post('/api/v1/auth/register',
+                         data=json.dumps(self.test_user),
+                         content_type='application/json')
+
         # create test user
         # self.response = self.client.post('/api/v1/auth/register',
         #                                  data=json.dumps(self.test_user),
@@ -37,17 +49,6 @@ class TestApi(TestCase):
 
     @unittest.skip("")
     def test_login(self):
-        test_user = {
-            "email": "edwin.kato@andela.com",
-            "first_name": "Edwin",
-            "last_name": "Kato",
-            "password": "qwerty@123",
-            "username": "EdwinKato"
-        }
-
-        self.client.post('/api/v1/auth/register',
-                         data=json.dumps(test_user),
-                         content_type='application/json')
         login_credentials = {
             "password": "qwerty@123",
             "username": "EdwinKato"
@@ -59,22 +60,36 @@ class TestApi(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_register(self):
-        test_user = {
-            "email": "edwin.kato@andela.com",
-            "first_name": "Edwin",
-            "last_name": "Kato",
+        new_test_user = {
+            "email": "john.seremba@andela.com",
+            "first_name": "John",
+            "last_name": "Seremba",
             "password": "qwerty@123",
-            "username": "EdwinKato"
+            "username": "JohnSeremba"
         }
 
+        count = len(User.query.all())
         response = self.client.post('/api/v1/auth/register',
-                                         data=json.dumps(test_user),
+                                         data=json.dumps(new_test_user),
                                          content_type='application/json')
+        new_count = len(User.query.all())
+        self.assertEqual(new_count - count, 1)
         self.assertEqual(response.status_code, 200)
 
-    @unittest.skip("")
     def test_add_bucket_list(self):
-        pass
+        bucket_list = {
+            "description": "Movies i have to watch by the end of the week",
+            "status": "Pending",
+            "title": "Entertainment",
+            "user_id": 1
+        }
+        count = len(BucketList.query.all())
+        response = self.client.post('/api/v1/bucketlists',
+                                         data=json.dumps(bucket_list),
+                                         content_type='application/json')
+        new_count = len(BucketList.query.all())
+        self.assertEqual(new_count - count, 1)
+        self.assertEqual(response.status_code, 200)
 
     @unittest.skip("")
     def test_get_bucket_lists(self):
