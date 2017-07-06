7babe92b2e0b12d9f3dc3c081359765a76ab3e6a
Edwin Kato <edwin.kato@andela.com>
2017-07-06 21:14:07 +0300

    Add test for register user

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,23 +1,76 @@
 import unittest
+from flask_testing import TestCase
 from flask import current_app, request, abort, jsonify, g, url_for
 
 from api.api import *
 from api.models import User
+from api import create_app, db
+import json
 
 
-class TestApi(unittest.TestCase):
+class TestApi(TestCase):
+
+    def create_app(self):
+
+        # pass in test configuration
+        return create_app("testing")
 
     def setUp(self):
+        # self.test_user = {
+        #     "email": "edwin.kato@andela.com",
+        #     "first_name": "Edwin",
+        #     "last_name": "Kato",
+        #     "password": "qwerty@123",
+        #     "username": "EdwinKato"
+        # }
+        db.create_all()
 
-        pass
+        # create test user
+        # self.response = self.client.post('/api/v1/auth/register',
+        #                                  data=json.dumps(self.test_user),
+        #                                  content_type='application/json')
+
+    def tearDown(self):
+
+        db.session.remove()
+        db.drop_all()
 
     @unittest.skip("")
     def test_login(self):
-        pass
+        test_user = {
+            "email": "edwin.kato@andela.com",
+            "first_name": "Edwin",
+            "last_name": "Kato",
+            "password": "qwerty@123",
+            "username": "EdwinKato"
+        }
 
-    @unittest.skip("")
+        self.client.post('/api/v1/auth/register',
+                         data=json.dumps(test_user),
+                         content_type='application/json')
+        login_credentials = {
+            "password": "qwerty@123",
+            "username": "EdwinKato"
+        }
+
+        response = self.client.post('/api/v1/auth/login',
+                                    data=json.dumps(login_credentials),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
     def test_register(self):
-        pass
+        test_user = {
+            "email": "edwin.kato@andela.com",
+            "first_name": "Edwin",
+            "last_name": "Kato",
+            "password": "qwerty@123",
+            "username": "EdwinKato"
+        }
+
+        response = self.client.post('/api/v1/auth/register',
+                                         data=json.dumps(test_user),
+                                         content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
     @unittest.skip("")
     def test_add_bucket_list(self):
