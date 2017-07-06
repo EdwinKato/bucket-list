e23e042cd804ef3026cf0061fe1e1b83ac190c64
Edwin Kato <edwin.kato@andela.com>
2017-07-06 21:36:07 +0300

    Add test for get bucket lists

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -91,9 +91,28 @@ class TestApi(TestCase):
         self.assertEqual(new_count - count, 1)
         self.assertEqual(response.status_code, 200)
 
-    @unittest.skip("")
     def test_get_bucket_lists(self):
-        pass
+        bucket_list_one = {
+            "description": "Movies i have to watch by the end of the week",
+            "status": "Pending",
+            "title": "Entertainment",
+            "user_id": 1
+        }
+        bucket_list_two = {
+            "description": "Places i have to travel to before the year ends",
+            "status": "Pending",
+            "title": "Leisure",
+            "user_id": 1
+        }
+        self.client.post('/api/v1/bucketlists',
+                        data=json.dumps(bucket_list_one),
+                         content_type='application/json')
+        self.client.post('/api/v1/bucketlists',
+                        data=json.dumps(bucket_list_two),
+                         content_type='application/json')
+        response = self.client.get('/api/v1/bucketlists')
+        self.assertIn("Movies i have to watch", str(response.data))
+        self.assertIn("Places i have to travel to", str(response.data))
 
     @unittest.skip("")
     def test_get_bucket_list(self):
