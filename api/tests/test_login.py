ad042127cadc2fd779bdea4d6853102b5d8d0ad0
Edwin Kato <edwin.kato@andela.com>
2017-07-07 12:04:18 +0300

    Add test for non-registered user

--- api/tests/test_login.py
+++ api/tests/test_login.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 
 from api.test import BaseTestCase
 
@@ -16,3 +17,21 @@ class TestLogin(BaseTestCase):
                                     data=json.dumps(login_credentials),
                                     content_type='application/json')
         self.assertEqual(response.status_code, 200)
+
+
+def test_non_registered_user_login(self):
+    """ Test for login of non-registered user """
+    with self.client:
+        response = self.client.post(
+            '/api/v1/auth/login',
+            data=json.dumps(dict(
+                email='edwin@andela.com',
+                password='123456'
+            )),
+            content_type='application/json'
+        )
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'fail')
+        self.assertTrue(response.content_type == 'application/json')
+        self.assertEqual(response.status_code, 404)
+
