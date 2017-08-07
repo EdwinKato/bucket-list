561dcc9b7804fede3e3c6cc76a0c374d97fabd87
Edwin Kato <edwin.kato@andela.com>
2017-08-07 17:32:17 +0300

    Add timeout on token expiration

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,18 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {
-  NgModule,
-  ApplicationRef
+	NgModule,
+	ApplicationRef
 } from '@angular/core';
 import {
-  removeNgStyles,
-  createNewHosts,
-  createInputTransfer
+	removeNgStyles,
+	createNewHosts,
+	createInputTransfer
 } from '@angularclass/hmr';
 import {
-  RouterModule,
-  PreloadAllModules
+	RouterModule,
+	PreloadAllModules
 } from '@angular/router';
+import { NgIdleKeepaliveModule } from '@ng-idle/keepalive';
+import { MomentModule } from 'angular2-moment';
 
 /*
  * Platform and Environment providers/directives/pipes
@@ -44,102 +46,104 @@ window["jQuery"] = $;
 
 // Application wide providers
 const APP_PROVIDERS = [
-  ...APP_RESOLVER_PROVIDERS,
-  AppState
+	...APP_RESOLVER_PROVIDERS,
+	AppState
 ];
 
 type StoreType = {
-  state: InternalStateType,
-  restoreInputValues: () => void,
-  disposeOldHosts: () => void
+	state: InternalStateType,
+	restoreInputValues: () => void,
+	disposeOldHosts: () => void
 };
 
 /**
  * `AppModule` is the main entry point into Angular2's bootstraping process
  */
 @NgModule({
-  bootstrap: [ AppComponent ],
-  declarations: [
-    AppComponent,
-    NoContentComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
+	bootstrap: [AppComponent],
+	declarations: [
+		AppComponent,
+		NoContentComponent,
+		LoginComponent,
+		RegisterComponent
+	],
   /**
    * Import Angular's modules.
    */
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    LayoutModule,
+	imports: [
+		BrowserModule,
+		FormsModule,
+		HttpModule,
+		LayoutModule,
+		MomentModule,
+		NgIdleKeepaliveModule.forRoot(),
     RouterModule.forRoot(ROUTES, { useHash: true, preloadingStrategy: PreloadAllModules })
-  ],
+	],
 
   /**
    * Expose our Services and Providers into Angular's dependency injection.
    */
-  providers: [
-    ENV_PROVIDERS,
-    APP_PROVIDERS
-  ]
+	providers: [
+		ENV_PROVIDERS,
+		APP_PROVIDERS
+	]
 })
 export class AppModule {
 
-  constructor(
-    public appRef: ApplicationRef,
-    public appState: AppState
-  ) {}
+	constructor(
+		public appRef: ApplicationRef,
+		public appState: AppState
+	) { }
 
-  public hmrOnInit(store: StoreType) {
-    if (!store || !store.state) {
-      return;
-    }
-    console.log('HMR store', JSON.stringify(store, null, 2));
+	public hmrOnInit(store: StoreType) {
+		if (!store || !store.state) {
+			return;
+		}
+		console.log('HMR store', JSON.stringify(store, null, 2));
     /**
      * Set state
      */
-    this.appState._state = store.state;
+		this.appState._state = store.state;
     /**
      * Set input values
      */
-    if ('restoreInputValues' in store) {
-      let restoreInputValues = store.restoreInputValues;
-      setTimeout(restoreInputValues);
-    }
+		if ('restoreInputValues' in store) {
+			let restoreInputValues = store.restoreInputValues;
+			setTimeout(restoreInputValues);
+		}
 
-    this.appRef.tick();
-    delete store.state;
-    delete store.restoreInputValues;
-  }
+		this.appRef.tick();
+		delete store.state;
+		delete store.restoreInputValues;
+	}
 
-  public hmrOnDestroy(store: StoreType) {
-    const cmpLocation = this.appRef.components.map((cmp) => cmp.location.nativeElement);
+	public hmrOnDestroy(store: StoreType) {
+		const cmpLocation = this.appRef.components.map((cmp) => cmp.location.nativeElement);
     /**
      * Save state
      */
-    const state = this.appState._state;
-    store.state = state;
+		const state = this.appState._state;
+		store.state = state;
     /**
      * Recreate root elements
      */
-    store.disposeOldHosts = createNewHosts(cmpLocation);
+		store.disposeOldHosts = createNewHosts(cmpLocation);
     /**
      * Save input values
      */
-    store.restoreInputValues  = createInputTransfer();
+		store.restoreInputValues = createInputTransfer();
     /**
      * Remove styles
      */
-    removeNgStyles();
-  }
+		removeNgStyles();
+	}
 
-  public hmrAfterDestroy(store: StoreType) {
+	public hmrAfterDestroy(store: StoreType) {
     /**
      * Display new elements
      */
-    store.disposeOldHosts();
-    delete store.disposeOldHosts;
-  }
+		store.disposeOldHosts();
+		delete store.disposeOldHosts;
+	}
 
 }
--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Idle, DEFAULT_INTERRUPTSOURCES } from '@ng-idle/core';
 
 @Component({
-    selector: 'dashboard-cmp',
-    templateUrl: 'dashboard.component.html'
+	selector: 'dashboard-cmp',
+	templateUrl: 'dashboard.component.html'
 })
 
-export class DashboardComponent{}
+export class DashboardComponent {
+	idleState = '';
+	timedOut = false;
+	lastPing?: Date = null;
+
+	constructor(private idle: Idle, private router: Router) {
+		idle.setIdle(600);
+		idle.setTimeout(1800);
+		idle.setInterrupts(DEFAULT_INTERRUPTSOURCES);
+
+		idle.onIdleEnd.subscribe(() => this.idleState = 'No longer idle.');
+		idle.onTimeout.subscribe(() => {
+			this.idleState = 'Timed out!';
+			this.timedOut = true;
+			router.navigateByUrl('/login');
+		});
+		idle.onIdleStart.subscribe(() => this.idleState = 'You\'ve gone idle!');
+		idle.onTimeoutWarning.subscribe(
+			(countdown) => this.idleState = 'You will time out in ' + countdown + ' seconds!');
+
+		this.reset();
+	}
+
+	reset() {
+		this.idle.watch();
+		this.idleState = '';
+		this.timedOut = false;
+	}
+}
